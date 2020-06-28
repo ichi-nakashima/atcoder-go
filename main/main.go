85411a074412f6dcbaed6cb315b063b255c9647e
ichi-nakashima <ichinaka3@gmail.com>
2020-06-28 12:19:38 +0900

    add ABC172AB

--- main/main.go
+++ main/main.go
@@ -2,24 +2,47 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	var N int
+	var M int
+	var K int
 	fmt.Scan(&N)
+	fmt.Scan(&M)
+	fmt.Scan(&K)
 
-	i := 1
-	num := N
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scan(&A[i])
+	}
+	B := make([]int, M)
+	for i := 0; i < M; i++ {
+		fmt.Scan(&B[i])
+	}
+	// todo2
 
-	flag := true
-	for flag {
-		b:= math.Pow(26, float64(i))
-		if num > int(b) {
-			num = num - int(b)
-			i++
-		} else {
-			flag = false
+
+	// todo1
+	count := 1
+	for {
+		var time int
+
+		if len(A) > 0 && A[0] <= B[0] {
+			time = A[0]
+			A = append(A[:0], A[1:]...)
+		} else if len(B) > 0 {
+			time = B[0]
+			B = append(B[:0], B[1:]...)
+		}
+		K -= time
+		if K < 0 {
+			break
+		}
+		count++
+		if len(A) == 0 && len(B) == 0 {
+			break
 		}
 	}
+	fmt.Println(count - 1)
 }
