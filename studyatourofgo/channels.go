0f094b92944397fcd55e122d9e7afa86979c21d8
ichi-nakashima <ichinaka3@gmail.com>
2020-07-12 10:21:26 +0900

    add other exercise

--- studyatourofgo/channels.go
+++ studyatourofgo/channels.go
@@ -11,5 +11,12 @@ func sum(s []int, c chan int) {
 }
 
 func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
 
+	c:=make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c
+
+	fmt.Println(x, y, x+y)
 }
